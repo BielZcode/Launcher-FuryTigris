1cff4b1296fe6b3fd69660f0b98607d1971c1694
Eduardo <eduardofreitasfr@gmail.com>
2025-01-10 16:51:56 -0300

    refactor: Adicionado token e versão

--- launcher.bat
+++ launcher.bat
@@ -1,2 +1,4 @@
 @echo off
-java -jar MinecraftLauncher.jar
+cd /d "%~dp0"
+java -Djava.library.path="%~dp0natives" -cp "%~dp0minecraft.jar;%~dp0libraries\*" net.minecraft.client.main.Main --accessToken 000 --version 1.8.8
+pause
